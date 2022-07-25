535649fab4a22e454ffe4a0196a266b6212bf3f2
Andrew Nourry <andrewnourry@gmail.com>
2022-07-25 20:44:32 +0200

    minor updates

--- optimized.py
+++ optimized.py
@@ -21,7 +21,7 @@ def get_best_profit(max_expense, cost, profit, names):
     result = table[n][max_expense]
     total_cost = []
 
-    print("Bénéfice sur 2 ans : ", result / 100)
+    print("Bénéfice sur 2 ans : ", result / 10000)
     print("liste des actions : ")
 
     for i in range(n, 0, -1):
@@ -38,13 +38,13 @@ def get_best_profit(max_expense, cost, profit, names):
             result = result - profit[i - 1]
             max_expense = max_expense - cost[i - 1]
 
-    print("Coût total : ", sum(total_cost))
+    print("Coût total : ", sum(total_cost) / 100)
 
 
 if __name__ == "__main__":
 
     """import csv file"""
-    file = open("data.csv")
+    file = open("dataset1_Python+P7.csv")
     csvreader = csv.reader(file)
 
     """append csv data to lists"""
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     stock_name = []
     stock_costs = []
     stock_profits = []
-    max_expense = 500
+    max_expense = 50000  # maximal cost * 100
 
     for data in csvreader:
         rows.append(data)
@@ -60,7 +60,15 @@ if __name__ == "__main__":
     for row, i in zip(rows, range(len(rows))):
         if int(float(rows[i][1])) > 0 and int(float(rows[i][2]) > 0):
             stock_name.append(rows[i][0])
-            stock_costs.append(int(float(rows[i][1])))
+            stock_costs.append(float(rows[i][1]))
             stock_profits.append(round(float(rows[i][2]) * float(rows[i][1])))
+
+    for cost, i in zip(stock_costs, range(len(stock_costs))):
+        stock_costs[i] = stock_costs[i] * 100
+        stock_costs[i] = int(stock_costs[i])
+
+    for profit, i in zip(stock_profits, range(len(stock_profits))):
+        stock_profits[i] = stock_profits[i] * 100
+        stock_profits[i] = int(stock_profits[i])
 
     get_best_profit(max_expense, stock_costs, stock_profits, stock_name)
